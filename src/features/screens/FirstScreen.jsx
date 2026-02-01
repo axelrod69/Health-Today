cacf98d9878f1a402ecb92fde956147bba94897b
Siddhartha Chatterjee <siddhartha.chatterjee@innofied.com>
2026-02-01 14:40:03 +0530

    Doctor App Push On 01-FEB-2026: Home Section In Progress

--- src/features/screens/FirstScreen.jsx
+++ src/features/screens/FirstScreen.jsx
@@ -1,10 +1,12 @@
+import Home from "../../shared/components/Home";
 import NavBar from "../../shared/components/NavBar";
 import '../styles/firstScreen.css'
 
 function FirstScreen() {
     return (
         <div className="firstScreen">
-            <NavBar/>
+            <NavBar />
+            <Home />
         </div>
     );
 }
---
+++ src/shared/components/Home.jsx
@@ -0,0 +1,19 @@
+import '../styles/home.css'
+
+function Home() {
+    return (
+        <>
+            <div className="intro">
+                <div className="firstSection">
+                    <div className='textBlock'>
+                        <h1>Better care for your health</h1>
+                        <p>The health and well-being of our patients will always be our priority. We follow the best practices for cleanliness</p>
+                    </div>
+                </div>
+                <div className="secondSection"></div>
+            </div>
+        </>
+    );
+}
+
+export default Home
